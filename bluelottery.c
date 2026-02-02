f2dbe6896e1d1932b4109aa5509a5e35496825d4
omairadhika-radhika <radhikaomai@gmail.com>
2026-02-02 16:00:24 +0530

    Add files via upload

---
+++ bluelottery.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("enter the value of a,b,c\n");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a+b==10||b+c==10||c+a==10)
+    {
+        printf("10");
+    }
+    else if (a>10||b>10||c>10)
+    {
+        printf("5");
+    }
+    else
+    {
+        printf("0");
+    }
+}
