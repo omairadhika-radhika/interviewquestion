45a210bf2d24e88a5489d50609bddf650644370a
omairadhika-radhika <radhikaomai@gmail.com>
2026-02-04 11:33:46 +0530

    Add files via upload

---
+++ row&col.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main() 
+{
+    int row;
+    int col;
+    printf("enter no of row:\n");
+    scanf("%d",&row);
+    printf("enter no of col:\n");
+    scanf("%d",&col);
+    for (int i=1;i<row;i++)
+    {
+        if (i==1||i==row||j++)
+        {
+            printf("*");
+        }
+        else
+        {
+            printf(" ");
+        }
+    }
+    printf("\n");
+        }
+        }
+ 
+ 6
