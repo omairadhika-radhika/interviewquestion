03720844178fce4d38b47e29ee8fe5e0ab7de28c
omairadhika-radhika <radhikaomai@gmail.com>
2026-02-04 11:05:30 +0530

    Add files via upload

---
+++ coloumns.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+
+void main() 
+{
+    int coloumns;
+    printf("enter no of coloumns");
+    scanf("%d",&coloumns);
+    for (int i=1;i<=coloumns;i++)
+{
+    printf("*\n");
+}
+}
